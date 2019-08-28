6f0848e80ed02bb18e2fd3171963efc870b9bf1d
Reema <it.reema7@gmail.com>
2019-08-29 00:09:04 +0300

    Update Day9.js

--- Day9/Day9.js
+++ Day9/Day9.js
@@ -1,16 +1,17 @@
-//semicolon required, two statements are on the same line.
-var a=3; var b=5;
-
-//semicolon optional, after statements it can be omitted if the statement is followed by a line break
-var a=3
-var b=5
-
-// semicolon optional, variable declaration in multiple lines
-var c
-c =
-10 
-document.write(c)
-
-// same as above but semicolon required, multiple statements on the same line.
-var c; c = 10; document.write(c);
-
+Math.pow(2,53) 
+Math.round(.6)
+Math.ceil(.6)
+Math.floor(.6)
+Math.abs(-5)
+Math.max(x,y,z)
+Math.min(x,y,z)
+Math.random() // x where 0 <= x < 1.0
+Math.PI // π: circumference of a circle / diameter(
+Math.E // e: The base of the natural logarithm
+Math.sqrt(3) // The square root of 3
+Math.pow(3, 1/3) // The cube root of 3
+Math.sin(0) // also Math.cos, Math.atan, etc.
+Math.log(10) // Natural logarithm of 10
+Math.log(100)/Math.LN10 // Base 10 logarithm of 100 
+Math.log(512)/Math.LN2 // Base 2 logarithm of 512 
+Math.exp(3) // Math.E cubed
