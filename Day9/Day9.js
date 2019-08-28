ee040f1166f174ea199ff266cc13f04c7543daff
Reema <it.reema7@gmail.com>
2019-08-28 13:18:03 +0300

    Types & Numbers

---
+++ Day9/Day9.js
@@ -0,0 +1,16 @@
+//semicolon required, two statements are on the same line.
+var a=3; var b=5;
+
+//semicolon optional, after statements it can be omitted if the statement is followed by a line break
+var a=3
+var b=5
+
+// semicolon optional, variable declaration in multiple lines
+var c
+c =
+10 
+document.write(c)
+
+// same as above but semicolon required, multiple statements on the same line.
+var c; c = 10; document.write(c);
+
---
+++ Day9/README.md
@@ -0,0 +1,2 @@
+# Day 9
+In Day 9 we learn about Types & Numbers in JavaScript.
