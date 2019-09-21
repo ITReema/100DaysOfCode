b30c437b23d2f3f3cd21c6f033a0f15100d9c636
Reema <it.reema7@gmail.com>
2019-09-21 20:12:37 +0300

    add files Day 32&33 ✅

---
+++ Day32&33/Day32&33.js
@@ -0,0 +1,15 @@
+try {
+  var x = Number(prompt("Please enter first number: "));
+  var y = Number(prompt("Please enter second number: "));
+  if ( y == 0 ){ 
+    throw( "Error: Divide by zero" ); 
+  } else { 
+    alert( "Divide " + x +" / " + y  + "= "+ x/y);
+  }
+}
+catch(err) {
+  alert(err);
+}
+finally{
+ console.log("using try/catch/finally in divide by zero!!");
+} 
---
+++ Day32&33/README.md
@@ -0,0 +1,5 @@
+# Day 32&33
+In Day 32&33 challenge, ```try```/```catch```/```finally```.
+
+* Write code:
+Using ```try```/```catch```/```finally``` in divide by zero.
